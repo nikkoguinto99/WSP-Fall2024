a21f4a95f8275d81df51cd13f13b046165fdb80e
Nikko <nikkoguinto@gmail.com>
2024-11-05 12:12:38 -0500

    Fixed Server to match what was performed in lecture

--- Assignments/final/Server/controllers/users.js
+++ Assignments/final/Server/controllers/users.js
@@ -1,33 +1,28 @@
-
-const model = require("../models/users")
+const model = require("../model/users")
 const express = require("express")
 const app = express.Router()
 
 app.get("/", (req, res) => {
-    model.getAll()
-}).get("/:id", (req, res) => {
-    const id = req.params.id //Local variable called id which points to the id parameter in the URL
-    const user = model.get(id)
-    res.send(user)
-}).post("/", (req, res) => {
-    const user = model.add(req.body)
-    data.items.push(user)
-    res.send(user)
-}).patch("/:id", (req, res) => { //patch edits information
-    const id = req.params.id
-    const user = model.update(id, req.body)
-    Object.assign(user, req.body)
-    res.send(user)
-}).delete("/:id", (req, res) => {
-    const id = req.params.id
-    const ret = model.remove(id)
-    res.send(user)
+    res.send(model.getAll())
 })
+    .get("/:id", (req, res) => {
+        const id = req.params.id
+        const user = model.get(id)
+        res.send(user)
+    })
+    .post("/", (req, res) => {
+        const user = model.add(req.body)
+        res.send(user)
+    })
+    .patch("/:id", (req, res) => {
+        const id = req.params.id
+        const user = model.update(id, req.body)
+        res.send(user)
+    })
+    .delete("/:id", (req, res) => {
+        const id = req.params.id
+        const ret = model.remove(id)
+        res.send(ret)
+    })
 
 module.exports = app
-/**
- * C - Create
- * R - Rewrite
- * U - Update
- * D - Delete
- */
--- Assignments/final/Server/index.js
+++ Assignments/final/Server/index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 const app = express()
+const userController = require("./controllers/users")
 
 const PORT = 3000
 
 app.get("/", (req, res) => {
-    res.send("Haven't forgotten a thing. Never will.")
-}).get("/about", (req, res) => {
+    res.send("Hello World")
+})
+    .get("/about", (req, res) => {
         res.send("About Us")
-    }).use("/users")
+    })
+    .use("/users", userController)
 
 app.listen(PORT, () => {
+    console.log("Haven't Forgotten a thing. Never will.")
     console.log("Server is running at http://localhost:" + PORT)
 })
