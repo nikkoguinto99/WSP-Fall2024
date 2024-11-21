2a186f8d18b6a31c9b3182a2af7c4f190ca8e305
Nikko <nikkoguinto@gmail.com>
2024-11-21 15:15:00 -0500

    Supabase Integration

---
+++ Assignments/final/Server/model/supabase.js
@@ -0,0 +1,9 @@
+const { createClient } =require("@supabase/supabase-js")
+
+module.exports = {
+    getConnection() {
+        return createClient(
+             process.env.SUPABASE_URL,
+             process.env.SUPABASE_SECRET_KEY)
+    },
+}
