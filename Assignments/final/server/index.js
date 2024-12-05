fe5789e6493185018684f5b69c9d284b125caecf
Nikko <nikkoguinto@gmail.com>
2024-12-05 14:23:08 -0500

    Handling Post and Patch

--- Assignments/final/server/index.js
+++ Assignments/final/server/index.js
@@ -1,9 +1,20 @@
 const express = require("express")
 const app = express()
+const userController = require("./controllers/users")
 const PORT = 3000
+
+// Middleware
+app.use(express.json())
+
+// Controllers
 app.get("/", (req, res) => {
-    res.send("Hello New Paltz")
+    res.send("Hello World")
 })
+    .get("/about", (req, res) => {
+        res.send("About Us")
+    })
+    .use("/users", userController)
+
 app.listen(PORT, () => {
     console.log("Server is running at http://localhost:" + PORT)
 })
