e9c268cbd1bb6d02780106e161272c25ba0e3738
Nikko <nikkoguinto@gmail.com>
2024-12-09 11:07:27 -0500

    Cors

--- Assignments/final/server/index.js
+++ Assignments/final/server/index.js
@@ -5,6 +5,12 @@ const postController = require("./controllers/posts")
 const PORT = 3000
 
 // Middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+})
 app.use(express.json())
 app.use(express.static(__dirname + "/dist")) //Run 'npm run build' from inside client folder to create dist folder in server
 
